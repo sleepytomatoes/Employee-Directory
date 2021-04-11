87367076bfaa3f561730da1dbd15bae57308e8f9
Shauna Dunn <shaunadunn1@gmail.com>
2021-04-11 18:45:41 -0400

    edited app.js, set up file structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Employees from "./pages/Employees";
+import SearchDepartment from "./pages/SearchDepartment";
+import SearchRole from "./pages/SearchRole";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/Employee-Directory" component={Home} />
+          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/about" component={About} /> */}
+          <Route exact path="/employees" component={Employees} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/searchDepartment" component={SearchDepartment} />
+          <Route exact path="/searchRole" component={SearchRole} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
